babd0eafa82013cef1b37c41d01b1928251dbc1e
erdemtoraman <erdemtoraman93@gmail.com>
2016-04-23 20:42:49 +0300

    calculate cosine similarity

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Timer;
 
 
 /**
@@ -9,15 +10,20 @@ import java.util.Iterator;
 public class Main {
     public static HashMap<Integer, Document> documents = new HashMap<>();
     public static HashMap<String, Double> vocabulary = new HashMap<>();
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        FileInputStream fileIn = new FileInputStream("tf-idf.ser");
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        documents = (HashMap<Integer, Document>) in.readObject();
-        in.close();
-        fileIn.close();
-        System.out.println(cosineSimilarity(documents.get(2288).getTfidfVector(), documents.get(2288).getTfidfVector()));
-        System.out.println(cosineSimilarity(documents.get(2288).getTfidfVector(), documents.get(2289).getTfidfVector()));
+        readFiles();
+        deserialize();
+
+
     }
+public static void deserialize() throws IOException, ClassNotFoundException {
+    FileInputStream fileIn = new FileInputStream("tf-idf.ser");
+    ObjectInputStream in = new ObjectInputStream(fileIn);
+    documents = (HashMap<Integer, Document>) in.readObject();
+    in.close();
+    fileIn.close();
+}
     public static void readFiles() throws IOException {
         BufferedReader read = new BufferedReader(new FileReader(new File("REUTERS_training_data.dat")));
         String str;
@@ -25,7 +31,7 @@ public class Main {
         boolean isConsidered;
         HashMap<String, Double> currentTf = new HashMap<>();
         while ((str = read.readLine()) != null) {
-            if(str.startsWith(".I ")){
+            if (str.startsWith(".I ")) {
                 currentDocId = Integer.parseInt(str.substring(str.indexOf(" ") + 1));
                 currentTf = new HashMap<>();
                 isConsidered = false;
@@ -34,16 +40,15 @@ public class Main {
             }
             if (str.equals("")) {
                 for (String w : currentTf.keySet()) {
-                    currentTf.put(w, currentTf.get(w)/(double)currentTf.keySet().size());
+                    currentTf.put(w, currentTf.get(w) / (double) currentTf.keySet().size());
                     if (vocabulary.keySet().contains(w)) {
                         vocabulary.put(w, vocabulary.get(w) + 1);
                     } else {
                         vocabulary.put(w, 1.0);
                     }
                 }
-                documents.put(currentDocId, new Document(currentDocId, currentTf));
-            }
-            else {
+                //  documents.put(currentDocId, new Document(currentDocId, currentTf));
+            } else {
                 String[] words = str.split(" ");
                 for (String w : words) {
                     if (currentTf.keySet().contains(w)) {
@@ -64,34 +69,16 @@ public class Main {
         }
         read.close();
     }
-    public static double cosineSimilarity(HashMap<String, Double> list1, HashMap<String, Double> list2){
-        Iterator itr1 = list1.keySet().iterator();
-        Iterator itr2 = list2.keySet().iterator();
-        double dividend = 0;
-        double lengthList1 = 0;
-        double lengthList2 = 0;
-        while(itr1.hasNext()){ // since their sizes are equal we dont have to check twice
-            String w1 = itr1.next().toString();
-            String w2 = itr2.next().toString();
-            double elem1 = list1.get(w1);
-            double elem2 = list2.get(w2);
-            if (list2.get(w2) != null){
-                dividend += elem1 * elem2;
-                lengthList1 += elem2 * elem2;
-                lengthList2 += elem1 * elem1;
-            }
-        }
-        return dividend/(Math.sqrt(lengthList1)*Math.sqrt(lengthList2));
-    }
 
-    public static void calculateAndAssigntfidf(){
-        for (int docId: documents.keySet()) {
+
+    public static void calculateAndAssigntfidf() {
+        for (int docId : documents.keySet()) {
             Document current = documents.get(docId);
-            for (String word : vocabulary.keySet()){
-                double idf = Math.log((double)documents.size() / vocabulary.get(word));
-                if (current.getTfidfVector().get(word) != null){
+            for (String word : vocabulary.keySet()) {
+                double idf = Math.log((double) documents.size() / vocabulary.get(word));
+                if (current.getTfidfVector().get(word) != null) {
                     double tf = current.getTfidfVector().get(word);
-                    current.getTfidfVector().put(word, tf*idf);
+                    current.getTfidfVector().put(word, tf * idf);
                 }
             }
         }
--- src/Rocchio.java
+++ src/Rocchio.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+
 /**
  * Created by Atakan Arıkan on 23.04.2016.
  */
 public class Rocchio {
+
+    public static HashMap<Integer, Document> documents = Main.documents;
+    public static HashMap<String, Double> vocabulary = Main.vocabulary;
+
+
+    public static double cosineSimilarity(HashMap<String, Double> list1, HashMap<String, Double> list2) {
+        double dividend = 0;
+        double lengthList1 = 0;
+        double lengthList2 = 0;
+// since their sizes are equal we dont have to check twice
+        for (String word : vocabulary.keySet()) {
+            if (list1.containsKey(word) && list2.containsKey(word)) {
+                double elem1 = list1.get(word);
+                double elem2 = list2.get(word);
+                dividend += elem1 * elem2;
+                lengthList1 += elem2 * elem2;
+                lengthList2 += elem1 * elem1;
+            }
+        }
+
+        return dividend / (Math.sqrt(lengthList1) * Math.sqrt(lengthList2));
+    }
+
 }
